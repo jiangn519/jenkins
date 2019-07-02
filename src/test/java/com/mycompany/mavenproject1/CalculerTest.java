9123fad6387a47b25b2123f469c76a4e60b5c1e7
usager <usager@10.0.2.15>
2019-07-02 12:16:04 -0400

    test jenkins 3

--- src/test/java/com/mycompany/mavenproject1/CalculerTest.java
+++ src/test/java/com/mycompany/mavenproject1/CalculerTest.java
@@ -39,7 +39,7 @@ public class CalculerTest {
 
     @org.junit.Test
     public void testSomeMethod() {
-        int expresult=50,resultat ;
+        int expresult=15,resultat ;
         Calculer c=new Calculer();
         resultat=c.add(5, 10);
         assertEquals(expresult,resultat);
@@ -52,7 +52,7 @@ public class CalculerTest {
     
     @org.junit.Test
     public void testSomeMethod1() {
-        int expresult=15,resultat ;
+        int expresult=50,resultat ;
         Calculer c=new Calculer();
         resultat=c.mul(5, 10);
         assertEquals(expresult,resultat);
